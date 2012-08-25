835f2a9e2e0c5cacf0f96fac78489c0c07506fb4
Boo <boobsd@gmail.com>
2012-08-25 18:15:19 +0300

    Typo fix

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ config = require('./config');
 redis = require('redis').createClient();
 
 
+periodic.checkAM2302() // Run first time after starting.
 var AM2302Timer = setInterval(periodic.checkAM2302, config.AM2302.timeout);
 
 console.log('Sensors started to collect data.');
