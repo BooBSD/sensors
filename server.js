23661a95c57a0c0e7fbfa09eb0e400769bb9da68
Boo <boobsd@gmail.com>
2012-08-25 19:40:32 +0300

    Fix connection to the redis server

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-var periodic = require('./periodic')
+var	periodic = require('./periodic')
 
 config = require('./config');
 redis = require('redis').createClient();
 
+redis.on('error', function(error) {
+	console.log(error);
+});
 
-periodic.checkAM2302() // Run first time after starting.
-var AM2302Timer = setInterval(periodic.checkAM2302, config.AM2302.timeout);
-
-console.log('Sensors started to collect data.');
+redis.on('ready', function() {
+	periodic.checkAM2302() // Run first time after starting.
+	var AM2302Timer = setInterval(periodic.checkAM2302, config.AM2302.timeout);
+	console.log('Sensors started to collect data.');
+});
