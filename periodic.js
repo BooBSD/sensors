3789f7320bec2653c48f6c4ddfc72e5a54aa34d5
Boo <boobsd@gmail.com>
2012-08-26 14:07:13 +0300

    Timestamp changes

--- periodic.js
+++ periodic.js
@@ -4,7 +4,7 @@ Date.prototype.timestamp = function() {
 	return Math.floor(this.getTime() / 1000)
 }
 
-exports.checkAM2302 = function() {
+exports.checkAM2302 = function(date) {
 	var getData = function() {
 		exec(config.AM2302.command, function(error, stdout, stderror) {
 			if(error) console.log(error)
@@ -12,8 +12,7 @@ exports.checkAM2302 = function() {
 			else {
 				var result = stdout.match(/Temp =\s+([0-9.]+).+Hum =\s+([0-9.]+)/im);
 				if(result) {
-					var ts = new Date().timestamp();
-					var data = ts + ',' + result[1] + ',' + result[2];
+					var data = date.timestamp() + ',' + result[1] + ',' + result[2];
 					redis.zadd('sensors:AM2302', ts, data, function(error) {
 						if(!error) clearInterval(getDataTimer);
 					});
